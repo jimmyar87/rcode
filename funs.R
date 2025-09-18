8012aba102478354d4a591d1d28a503ddc04b348
jimmyar87 <109400715+jimmyar87@users.noreply.github.com>
2025-09-18 18:37:34 +0200

    Update funs.R

--- funs.R
+++ funs.R
@@ -1,12 +1,10 @@
-
-# divide by two function
-
-f_div <- function(number) {
-  number / 2
-}
-
-# multiply by two function
-
-f_mul <- function(number) {
-  number * 2
+read.btb <- function(file) {
+  btbdata <- read.table(file = file, skip = 16, header = FALSE)
+  btbdata <- subset(btbdata, select = -c(2, 10, 11, 13, 14))
+  colnames(btbdata) <- c("time", "vt", "ti", "te", "pico2", "peco2", "pio2", "peo2", "so2")
+  btbdata$ve <- (btbdata$"vt" / 1000) * (60000 / (btbdata$"ti" + btbdata$"te"))
+  btbdata$rr <- (60000 / (btbdata$"ti" + btbdata$"te"))
+  btbdata$time <- as.POSIXct(btbdata$time, format = "%H:%M:%OS")
+  btbdata <- btbdata[, c("time", "ve", "rr", "vt", "ti", "te", "pico2", "peco2", "pio2", "peo2", "so2")]
+  return(btbdata)
 }
